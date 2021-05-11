36a4de9e2ec6691fef2661d667477fa8b7216857
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-05-11 09:49:49 +0900

    update: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { GlobalStyle } from 'src/styles';
-import { Home } from 'src/components/pages';
+import { Detail } from 'src/components/pages';
 
 const App: React.VFC = () => {
   return (
     <>
       <GlobalStyle />
-      <div>App</div>
-      <Home />
+      <Detail />
     </>
   );
 };
