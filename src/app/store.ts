ad568208bf6f807003eaae93181bd336ace592d3
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-05-13 12:42:06 +0900

    add: posts slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import postsReducer from 'src/features/posts/postsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    posts: postsReducer,
   },
 });
 
