6be1a6ee8d22a2b23e1b256be348f914f3f12048
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-05-16 18:11:30 +0900

    refactor: no post message

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -4,11 +4,20 @@ import { DefaultLayout } from 'src/components/template';
 
 import { useAppSelector } from 'src/app/hooks';
 import { selectPosts } from 'src/features/posts/postsEntitySlice';
+import ErrorMessage from 'src/components/molecules/ErrorMessage';
 
 const Home: React.VFC = memo(() => {
   const posts = useAppSelector(selectPosts.selectAll);
 
-  return <DefaultLayout>{posts && <PostItemList posts={posts} />}</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      {posts ? (
+        <PostItemList posts={posts} />
+      ) : (
+        <ErrorMessage>記事が投稿されていません。</ErrorMessage>
+      )}
+    </DefaultLayout>
+  );
 });
 
 export default Home;
